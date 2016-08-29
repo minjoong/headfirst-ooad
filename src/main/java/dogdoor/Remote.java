672b67c47385ddd75a80d4783c74d1014482d635
Minjoong Kim <minjoong.slow@gmail.com>
2016-08-30 00:47:33 +0900

    강아지 문 자동 닫힘 기능을 Remote에서 DogDoor 클래스로 이동

--- src/main/java/dogdoor/Remote.java
+++ src/main/java/dogdoor/Remote.java
@@ -1,8 +1,5 @@
 package dogdoor;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 public class Remote {
 	
 	private DogDoor door;
@@ -17,14 +14,6 @@ public class Remote {
 			door.close();
 		} else {
 			door.open();
-			
-			final Timer timer = new Timer();
-			timer.schedule(new TimerTask() {
-				public void run() {
-					door.close();
-					timer.cancel();
-				}
-			}, 5000);
 		}
 	}
 
