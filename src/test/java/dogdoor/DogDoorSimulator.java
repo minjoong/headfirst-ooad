aa91088a7945095345df06a42794117fadd2f338
Minjoong Kim <minjoong.slow@gmail.com>
2016-08-29 23:34:07 +0900

    대체 경로에 대해 테스트하도록 JUnit 수정

--- src/test/java/dogdoor/DogDoorSimulator.java
+++ src/test/java/dogdoor/DogDoorSimulator.java
@@ -29,12 +29,30 @@ public class DogDoorSimulator {
 	
 	@Test
 	public void test() {
-		remote.pressButton();
+		// 피도가 나가기 위해 짖는다.
+		// 토드 또는 지나가 피도가 짖는 것을 듣는다.
+		remote.pressButton(); // 토드 또는 지나가 리모콘의 버튼을 누른다.
 		assertEquals(outContent.toString(), "Pressing the remote control button...\nThe dog door opens.\n");
-		
 		outContent.reset();
+		
 		try {
-			Thread.sleep(6000);
+			Thread.sleep(6000); // 피도가 나가서, 볼 일을 보고, 논다.
+			// 문이 자동으로 닫힌다. 5초 후..
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		assertEquals(outContent.toString(), "The dog door closes.\n");
+		outContent.reset();
+		
+		// 피도가 들어오기 위해 짖는다.
+		// 토드 또는 지나가 피도가 짖는 것을 듣는다.
+		remote.pressButton(); // 토드 또는 지나가 리모콘의 버튼을 누른다.
+		assertEquals(outContent.toString(), "Pressing the remote control button...\nThe dog door opens.\n");
+		outContent.reset();
+		
+		try {
+			Thread.sleep(6000); // 피도가 들어온다.
+			// 문이 자동으로 닫힌다. 5초 후..
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
